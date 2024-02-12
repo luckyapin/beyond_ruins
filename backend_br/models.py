771057de383182287570ddc961281b0740dc204a
Gorlov Andrey <a.b.gorlov@gmail.com>
2024-02-12 22:11:07 +0300

    working_test_carefull

--- backend_br/models.py
+++ backend_br/models.py
@@ -3,14 +3,11 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 # БД на ORM
-class Categories(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
 
 class Posts(models.Model):
+    class Meta:
+        app_label = 'backend_br'
+
     title = models.CharField(max_length=100)
     text = models.TextField()
     creationTime = models.DateTimeField(auto_now_add=True)
@@ -21,8 +18,19 @@ class Posts(models.Model):
     def __str__(self):
         return self.title
 
+class Categories(models.Model):
+    class Meta:
+        app_label = 'backend_br'
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Comments(models.Model):
+    class Meta:
+        app_label = 'backend_br'
+
     commentText = models.TextField()
     creationTime = models.DateTimeField(auto_now_add=True)
     userId = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
