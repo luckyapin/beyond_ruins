1834052dbea3fd5d57b89c54ad06c1fd2b04625f
Gorlov Andrey <a.b.gorlov@gmail.com>
2024-02-13 20:41:37 +0300

    admin can now

--- backend_br/permissions.py
+++ backend_br/permissions.py
@@ -4,12 +4,15 @@ from rest_framework import permissions
 # Ограничение к большинству запросов
 class MyPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):  # К единичному
-
+        if request.user.is_superuser:  # Проверяем, является ли пользователь суперпользователем
+            return True
         if request.method in permissions.SAFE_METHODS:  # Разрешаем все безопасные
             return True
         return obj.userId == request.user  # Создатель может делать со своим творением что-угодно
 
     def has_permission(self, request, view):  # К нескольким экземплярам
+        if request.user.is_superuser:  # Проверяем, является ли пользователь суперпользователем
+            return True
         if request.method in permissions.SAFE_METHODS:  # Разрешаем все безопасные
             return True
         return bool(request.user.is_authenticated)
@@ -20,10 +23,14 @@ class MyPermission(permissions.BasePermission):
 # Ограничение для таблицы User. Можно только смотреть, добавление - это регистрация
 class UserPermission(permissions.BasePermission):
     def has_permission(self, request, view):
+        if request.user.is_superuser:  # Проверяем, является ли пользователь суперпользователем
+            return True
         if request.method in permissions.SAFE_METHODS:
             return True
         return False
 
     def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:  # Проверяем, является ли пользователь суперпользователем
+            return True
         if request.method in permissions.SAFE_METHODS:
             return True
--- backend_br/tests.py
+++ backend_br/tests.py
@@ -3,12 +3,54 @@ from .views import PostsViewSet
 from django.urls import reverse
 from rest_framework import status
 from beyond_ruins_project import urls
+from django.contrib.auth import get_user_model
+import json
+
+user = get_user_model()
 
 
 class PostViewSetTestCase(APITestCase):
     def setUp(self):
         self.url = reverse('posts-list')
 
+    def authenticate(self):
+        data = {
+            'username': "testuser",
+            'password': "testpassword",
+            'email': "testemail@mail.com"
+        }
+        data2 = {
+            'username': "testuser",
+            'password': "testpassword",
+        }
+
+        self.client.post('/api/v1/auth/users/', data=json.dumps(data), content_type='application/json')
+        response = self.client.post(reverse('token_obtain_pair'), data=json.dumps(data2),
+                                    content_type='application/json')
+        token = response.data['access']
+
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
+
     def test_view_posts(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_creation(self):
+        sample_post = {
+            'title': "testpost",
+            'text': 'asd',
+            'userId': '1'
+        }
+        self.authenticate()
+        response = self.client.post(reverse('posts-list'), json.dumps(sample_post), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        print(response.data)
+        self.assertEqual(response.data['title'], 'testpost')
+
+    def test_post_deletion(self):
+        self.authenticate()
+        post_id = 1
+        delete_url = f"/api/v1/Posts/{post_id}/"
+        response = self.client.delete(delete_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        # 'posts-detail', kwargs={'pk': post_id}
